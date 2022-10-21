51b0c87596ae581b20a2f480eb4749114cc99cb1
Ivan <clicker.heroes.acg@gmail.com>
2022-10-21 22:15:34 +0300

    Initial commit

---
+++ src/main/java/org/xojo/Xojo.java
@@ -0,0 +1,51 @@
+package org.xojo;
+
+import java.util.Objects;
+
+public class Xojo {
+  private final String body;
+
+  public Xojo(final String body) {
+    this.body = body;
+  }
+
+  public Xojo addNode(final String node) throws IllegalAccessException {
+    this.checkNode(node);
+    return new Xojo(this.body + "/" + node);
+  }
+
+  public Xojo addAttr(final String key, final String value) throws IllegalAccessException {
+    this.checkNode(key);
+    return new Xojo(
+        this.body.concat(
+            String.format(
+                "[@%s=\"%s\"]",
+                key,
+                value
+            )
+        )
+    );
+  }
+
+  public Xojo addAttrs(final String node) throws IllegalAccessException {
+    this.checkNode(node);
+    return new Xojo(this.body + "@" + node);
+  }
+
+  @Override
+  public String toString() {
+    return this.body;
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (!obj.getClass().equals(Xojo.class)) return false;
+    else return Objects.equals(Xojo.class.cast(obj).body, this.body);
+  }
+
+  private void checkNode(final String node) throws IllegalAccessException {
+    if (node.isEmpty() || node.isBlank()) {
+      throw new IllegalAccessException("Node can't be empty!");
+    }
+  }
+}
---
+++ src/test/java/org/xojo/XojoTest.java
@@ -0,0 +1,39 @@
+package org.xojo;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class XojoTest {
+  final String parent = "parent";
+  final String child = "child";
+
+  Xojo xj;
+
+  @BeforeEach
+  void setUp() {
+    this.xj = new Xojo(this.parent);
+  }
+
+  @Test
+  void addNodeSuccess() throws IllegalAccessException {
+    final Xojo expected = new Xojo("parent/child");
+    final Xojo actual = this.xj.addNode(this.child);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void addNodeFailsIllegalStateException() {
+    assertThrows(IllegalAccessException.class, () -> this.xj.addNode(""));
+  }
+
+  @Test
+  void addAttrSuccess() throws IllegalAccessException {
+    final Xojo expected = new Xojo("parent[@key=\"value\"]");
+    final Xojo actual = this.xj.addAttr("key", "value");
+    assertEquals(expected, actual);
+  }
+}
