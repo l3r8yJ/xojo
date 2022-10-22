4cb8a56c54a29891bb0abbd7a618908942bfa4ab
Ivan <clicker.heroes.acg@gmail.com>
2022-10-22 16:26:05 +0300

    addNodes, addProperty, all

--- src/main/java/org/xojo/Xojo.java
+++ src/main/java/org/xojo/Xojo.java
@@ -2,31 +2,39 @@ package org.xojo;
 
 import java.util.Objects;
 
-public class Xojo {
+public final class Xojo {
   private final String body;
 
   public Xojo(final String body) {
     this.body = body;
   }
 
-  public Xojo addNode(final Node node) {
-    return new Xojo(this.body + "/" + node.value());
-  }
-
   public Xojo addAttr(final Attr attr) {
-    return new Xojo(
-      this.body.concat(
-        String.format(
-          "[@%s=\"%s\"]",
-          attr.key(),
-          attr.value()
-        )
-      )
-    );
+    return new Xojo(String.format("%s[@%s=\"%s\"]", this.body, attr.key(), attr.value()));
   }
 
   public Xojo addAttrs(final Node node) {
-    return new Xojo(this.body + "@" + node.value());
+    return new Xojo(this.formatted("@%s", node));
+  }
+
+  public Xojo addNode(final Node node) {
+    return new Xojo(this.formatted("/%s", node));
+  }
+
+  public Xojo addNodes(final Node node) {
+    return new Xojo(this.formatted("//%s", node));
+  }
+
+  public Xojo addProperty(final Node node) {
+    return new Xojo(this.formatted("%s()", node));
+  }
+
+  public Xojo all() {
+    return new Xojo(String.format("%s*", this.body));
+  }
+
+  public Xojo at(final int pos) {
+    return new Xojo(String.format("%s[%d]", this.body, pos));
   }
 
   @Override
@@ -38,6 +46,10 @@ public class Xojo {
   public boolean equals(final Object obj) {
     if (null == obj) throw new IllegalArgumentException("Object can't be a null!");
     if (!obj.getClass().equals(Xojo.class)) return false;
-    else return Objects.equals(Xojo.class.cast(obj).body, this.body);
+    else return Objects.equals(((Xojo) obj).body, this.body);
+  }
+
+  private String formatted(final String format, final Node node) {
+    return String.format("%s".concat(format), this.body, node.value());
   }
 }
